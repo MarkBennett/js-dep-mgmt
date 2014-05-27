1c82500890f75eb98f47c1b7468ca9d7d1a132f5
Mark Bennett <mark@burmis.ca>
2014-05-27 09:57:56 -0600

    README with notes for my talk

--- 3-browserify/app.js
+++ 3-browserify/app.js
@@ -1,14 +1,3 @@
-// To run this code:
-//
-//   npm install -g browserify
-//   npm install
-//   browserify app.js -o bundle.js
-//
-// You can require node modules, as well as any of the standard node
-// libraries listed here:
-//
-//    https://github.com/substack/node-browserify#compatibility
-
 var greet = require("./greeter").greet;
 var print = require("util").print;
 
